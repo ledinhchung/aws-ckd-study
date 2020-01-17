47badc0757f92c7971f28c47413eba6e16273ee8
Chung Le <chungld@pascaliaasia.com>
2020-01-17 16:53:56 +0700

    First try with AWS CDK

--- lib/ecs-test-stack.ts
+++ lib/ecs-test-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecsPattens from '@aws-cdk/aws-ecs-patterns'
 
 export class EcsTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'ecs-vpc', {
+      maxAzs: 3,
+      cidr: '172.10.0.0/24',
+    });
+
+    const cluster = new ecs.Cluster(this, 'EcsCluster', {
+      vpc,
+      clusterName: 'EcsCluster'
+    });
+
+    const ecs_patterns = new ecsPattens.ApplicationLoadBalancedFargateService(this, 'FirstFargateService', {
+      cluster,
+      cpu: 512,
+      memoryLimitMiB: 1024,
+      publicLoadBalancer: true,
+      desiredCount: 4,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("nginx") }
+    });
   }
 }
