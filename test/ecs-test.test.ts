d12e2d85c462f629419c8b83517e62d1dcb3f15a
Chung Le <ledinhchung.it@gmail.com>
2020-01-20 15:58:28 +0700

    Implement test to confirm AWS resources

--- test/ecs-test.test.ts
+++ test/ecs-test.test.ts
@@ -1,13 +1,30 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
 import * as cdk from '@aws-cdk/core';
+
 import EcsTest = require('../lib/ecs-test-stack');
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new EcsTest.EcsTestStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+const app = new cdk.App();
+// WHEN
+const stack = new EcsTest.EcsTestStack(app, 'MyTestStack');
+
+test('Network setting', () => {
+  // THEN
+  expect(stack).toHaveResource('AWS::EC2::VPC', {
+    CidrBlock: '172.10.0.0/24'
+  })
 });
+
+test('ECS cluster', () => {
+  // THEN
+  expect(stack).toHaveResource('AWS::ECS::Cluster', {
+    ClusterName: 'EcsCluster'
+  })
+})
+
+test('Task definition', () => {
+  expect(stack).toHaveResource('AWS::ECS::TaskDefinition', {
+    Cpu: '256',
+    Memory: '512',
+  })
+})
